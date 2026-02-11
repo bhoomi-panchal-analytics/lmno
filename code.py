77ef76fc21a14447256c118dff231987c5c5e675
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-11 18:48:51 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
